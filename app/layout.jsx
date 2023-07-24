026544a9dc57a288c6bb8694c288d6c248bef141
julien-muke <julien.muke@gmail.com>
2023-07-24 18:16:50 +0200

    home page

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,8 @@
 import '@styles/globals.css';
 
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
+
 export const metadata = {
     title: "SharePrompts",
     description: 'Discover & Share AI Prompts'
@@ -13,6 +16,7 @@ const RootLayout = ({ children }) => {
                 <div className='gradient' />
             </div>
             <main className='app'>
+                <Nav />
                 {children}
             </main>
         </body>
@@ -20,4 +24,4 @@ const RootLayout = ({ children }) => {
   )
 }
 
-export default ootLayout
+export default RootLayout
