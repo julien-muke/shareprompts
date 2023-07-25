96aeb7b58a0cbbf475fcb63b89d8c8750fe51d5b
julien-muke <julien.muke@gmail.com>
2023-07-25 11:13:01 +0200

    google auth

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,27 +1,28 @@
-import '@styles/globals.css';
+import "@styles/globals.css";
 
 import Nav from "@components/Nav";
 import Provider from "@components/Provider";
 
 export const metadata = {
-    title: "SharePrompts",
-    description: 'Discover & Share AI Prompts'
-}
+  title: "SharePrompts",
+  description: "Discover & Share AI Prompts",
+};
 
-const RootLayout = ({ children }) => {
-  return (
-    <html lang='en'>
-        <body>
-            <div className='main'>
-                <div className='gradient' />
-            </div>
-            <main className='app'>
-                <Nav />
-                {children}
-            </main>
-        </body>
-    </html>
-  )
-}
+const RootLayout = ({ children }) => (
+  <html lang='en'>
+    <body>
+      <Provider>
+        <div className='main'>
+          <div className='gradient' />
+        </div>
 
-export default RootLayout
+        <main className='app'>
+          <Nav />
+          {children}
+        </main>
+      </Provider>
+    </body>
+  </html>
+);
+
+export default RootLayout;
